6476b672af84544487e0e4c351930724f859371a
Ciupix22 <ciprian.teletin99@e-uvt.ro>
2021-01-01 22:28:38 +0200

    Product page finished.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {CartService} from '../../services/cart.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {map} from 'rxjs/operators';
+import {ProductModelServer} from '../../models/product.model';
+
+declare let $: any;
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, AfterViewInit {
+  id: number;
+  product: ProductModelServer;
+  @ViewChild('quantity') quantityInput;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService,
+              private cartService: CartService,
+              private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map((param: ParamMap) => {
+        // @ts-ignore
+        return param.params.id;
+      })
+    ).subscribe(prodId => {
+      this.id = prodId;
+      this.productService.getSingleProduct(this.id).subscribe(prod => {
+        this.product = prod;
+      });
+    });
+  }
+
+  ngAfterViewInit(): void {
+// Product Main img Slick
+    $('#product-main-img').slick({
+      infinite: true,
+      speed: 300,
+      dots: false,
+      arrows: true,
+      fade: true,
+      asNavFor: '#product-imgs',
+    });
+
+    // Product img zoom
+    const zoomMainProduct = document.getElementById('product-main-img');
+    if (zoomMainProduct) {
+      $('#product-main-img .product-preview').zoom();
+    }
+
+  }
+
+  increase() {
+    // tslint:disable-next-line:radix
+    let value = parseInt(this.quantityInput.nativeElement.value);
+    if (this.product.quantity >= 1) {
+      value++;
+    }
+
+    if (value > this.product.quantity) {
+      value = this.product.quantity;
+      return;
+    }
+
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  decrease() {
+    // tslint:disable-next-line:radix
+    let value = parseInt(this.quantityInput.nativeElement.value);
+    if (this.product.quantity >= 1) {
+      value--;
+    }
+
+    if (value < 1) {
+      value = 1;
+      return;
+    }
+
+    this.quantityInput.nativeElement.value = value.toString();
+  }
+
+  addToCart(id: number) {
+    this.cartService.addProductToCart(id, this.quantityInput.nativeElement.value);
+  }
 }
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -73,27 +73,6 @@
     asNavFor: '#product-imgs',
   });
 
-	// Product imgs Slick
-  $('#product-imgs').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    centerMode: true,
-    focusOnSelect: true,
-		centerPadding: 0,
-		vertical: true,
-    asNavFor: '#product-main-img',
-		responsive: [{
-        breakpoint: 991,
-        settings: {
-					vertical: false,
-					arrows: false,
-					dots: true,
-        }
-      },
-    ]
-  });
-
 	// Product img zoom
 	var zoomMainProduct = document.getElementById('product-main-img');
 	if (zoomMainProduct) {
